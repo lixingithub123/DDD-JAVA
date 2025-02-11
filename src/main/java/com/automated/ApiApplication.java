bbe509073b1623657df9c4365f5900a78676a023
lixingithub123 <shinisbigdevil@hotmail.com>
2025-02-11 17:14:52 +0800

    初始化基础逻辑

--- src/main/java/com/automated/ApiApplication.java
+++ src/main/java/com/automated/ApiApplication.java
@@ -1,13 +1,18 @@
-package com.automated.api;
+package com.automated;
 
+import cn.dev33.satoken.SaManager;
+import org.dromara.autotable.springboot.EnableAutoTable;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAutoTable
 @SpringBootApplication
 public class ApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiApplication.class, args);
+		System.out.println("启动成功，Sa-Token 配置如下：" + SaManager.getConfig());
 	}
 
 }
